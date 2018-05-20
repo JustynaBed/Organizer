1658dcece496bcc8525c9d8ceac0a7a25c8a2de0
JustynaBed <justyna.bednarko@gmail.com>
2018-05-20 17:32:41 +0200

    firebase authentication

---
+++ js/login.js
@@ -0,0 +1,53 @@
+const LoginPanel = document.getElementById('login-panel');
+const UserPanel = document.getElementById('user-panel');
+const btnSignIn = document.getElementById('btn-sign-in');
+const btnSignUp = document.getElementById('btn-sign-up');
+const btnSignOut = document.getElementById('btn-sign-out');
+const txtEmailSignIn = document.getElementById('email-sign-in');
+const txtPasswordSignIn = document.getElementById('password-sign-in');
+const txtEmailSignUp = document.getElementById('email-sign-up');
+const txtPasswordSignUp = document.getElementById('password-sign-up');
+
+btnSignIn.addEventListener('click', e => {
+const emailIn = txtEmailSignIn.value;
+const passIn = txtPasswordSignIn.value;
+const auth = firebase.auth();
+
+const promise = auth.signInWithEmailAndPassword(emailIn, passIn);
+promise.catch(e => console.log(e.massage));
+console.log('Kliknieto')
+});
+
+btnSignUp.addEventListener('click', e => {
+const emailUp = txtEmailSignUp.value;
+const passUp = txtPasswordSignUp.value;
+const auth = firebase.auth();
+
+const promise = auth.createUserWithEmailAndPassword(emailUp, passUp);
+promise.catch(e => console.log(e.massage));
+console.log('Kliknieto')
+});
+
+btnSignOut.addEventListener('click', e => {
+firebase.auth().signOut();
+});
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+if(firebaseUser) {
+console.log("User: ", firebaseUser);
+btnSignOut.classList.remove('hide');
+UserPanel.style.display = "block";
+LoginPanel.style.display = "none";
+
+const user = firebase.auth().currentUser;
+
+if(user != null) {
+document.getElementById('login-panel-text').innerHTML = 'Welcome User : ' + user.email
+}
+} else {
+console.log('no one is logged in');
+btnSignOut.classList.add('hide');
+UserPanel.style.display = "none";
+LoginPanel.style.display = "block";
+}
+});
