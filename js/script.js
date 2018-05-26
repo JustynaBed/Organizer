a326b8693216877aaafcc4782475afff847c7880
JustynaBed <justyna.bednarko@gmail.com>
2018-05-26 23:53:09 +0200

    validate form sign up

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ const $formSignIn = $('.form-sign-in');
 const $formSignUp = $('.form-sign-up');
 const $tabSignIn = $('.sign-in-tab');
 const $tabSignUp = $('.sign-up-tab');
+const $emailSignUp = $('#email-sign-up');
+const $passwordSignUp = $('#password-sign-up');
+const $btnSignUp = $('#btn-sign-up');
+
+const REQUIRED_PATTERN_MAIL = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+const REQUIRED_PATTERN_PASSWORD = /^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/;
 
 $(document).ready(function() {
     $formSignUp.hide();
@@ -23,3 +29,14 @@ $tabSignUp.on('click', function() {
     $formSignIn.hide();
     $formSignUp.show();
 });
+
+function required() {
+    return !REQUIRED_PATTERN_MAIL.test($emailSignUp.val()) ||
+    !REQUIRED_PATTERN_PASSWORD.test($passwordSignUp.val()) 
+}
+
+function validatebtnSignUp() {
+    $btnSignUp.prop('disabled', required());
+}
+
+$emailSignUp.add($passwordSignUp).on('keyup', validatebtnSignUp);
