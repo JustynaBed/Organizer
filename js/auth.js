e36aed7f9807c6be550322d5f934a9c2031f8a98
JustynaBed <justyna.bednarko@gmail.com>
2018-08-17 16:09:24 +0200

    add firebase

---
+++ js/auth.js
@@ -0,0 +1,9 @@
+        var config = {
+            apiKey: "AIzaSyClIzgbB_tQAYZKAbxd7eblBIyLBbtDi2U",
+        authDomain: "projectone-8ba5b.firebaseapp.com",
+        databaseURL: "https://projectone-8ba5b.firebaseio.com",
+        projectId: "projectone-8ba5b",
+        storageBucket: "projectone-8ba5b.appspot.com",
+        messagingSenderId: "342600999285"
+      };
+      firebase.initializeApp(config);
